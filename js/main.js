ee55a6a26721270a4bfc14e8e46c4783f6367e7b
Sal Tamay <sal.tamay@outlook.com>
2019-11-14 15:36:39 -0500

    Add hover effects to skills-cards using js events

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ function displayMenu() {
 }
 
 menuBar.addEventListener('click', displayMenu);
+
+// Add mouseover event listeners to skill-card items
+document.querySelectorAll('.skills-card').forEach(card => {
+  card.addEventListener('mouseenter', e => {
+    e.target.style.backgroundColor = '#f94f5b';
+    document.querySelector('#' + e.target.id + ' .skills-card-title').style.color = '#fff';
+    document.querySelector('#' + e.target.id + ' .skills-card-description').style.color = '#fff';
+    document.querySelector('#' + e.target.id + ' .skills-card-link').style.color = '#fff';
+
+    if (e.target.id !== 'html') {
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg path').setAttribute('fill', '#fff');
+    }
+
+    if (e.target.id === 'react') {
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg path').setAttribute('fill', '#fff');
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg g').setAttribute('fill', '#fff');
+    }
+  });
+  card.addEventListener('mouseleave', e => {
+    e.target.style.backgroundColor = '#fff';
+    document.querySelector('#' + e.target.id + ' .skills-card-title').style.color = '#000';
+    document.querySelector('#' + e.target.id + ' .skills-card-description').style.color = '#000';
+    document.querySelector('#' + e.target.id + ' .skills-card-link').style.color = '#000';
+
+    if (e.target.id !== 'html') {
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg path').setAttribute('fill', '#000');
+    }
+
+    if (e.target.id === 'react') {
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg path').setAttribute('fill', '#000');
+      document.querySelector('#' + e.target.id + ' .skills-card-icon svg g').setAttribute('fill', '#000');
+    }
+  })
+})
