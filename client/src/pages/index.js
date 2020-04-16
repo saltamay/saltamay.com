e775432c51279b82573428690a8dd35ce412af30
Sal Tamay <sal.tamay@outlook.com>
2020-04-16 17:23:52 -0400

    feat: Add landing page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -14,13 +14,55 @@ const IndexPage = () => (
       <div className="inner">
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
-        <ul className="actions special">
+        {/* <ul className="actions special">
           <li>
             <Scroll type="id" element="one">
               <a href="/#" className="button primary">
                 Explore
               </a>
             </Scroll>
+          </li>
+        </ul> */}
+        <ul className="social">
+          <li>
+            <a
+              className="fab fa-facebook-f"
+              // href={data.facebook}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              className="fab fa-twitter"
+              // href={data.twitter}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              className="fab fa-instagram"
+              // href={data.instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              className="fab fa-linkedin-in"
+              // href={data.linkdin}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </li>
+          <li>
+            <a
+              className="fab fa-github"
+              // href={data.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
           </li>
         </ul>
       </div>
